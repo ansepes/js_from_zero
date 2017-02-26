22d0c8c78e12db9a8f92eab7c6ef99803daa8ac2
User2 <User2>
2017-02-26 16:29:53 +0900

    add mocha, chai, and sinon

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,12 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
 
+import mocha from 'gulp-mocha';
 import gulp from 'gulp';
 import babel from 'gulp-babel';
 import del from 'del';
 import eslint from 'gulp-eslint';
 import webpack from 'webpack-stream';
 import webpackConfig from './webpack.config.babel';
+
 
 const paths = {
   allSrcJs: 'src/**/*.js?(x)',
@@ -18,6 +20,7 @@ const paths = {
   libDir: 'lib',
   distDir: 'dist',
   clientBundle: 'dist/client-bundle.js?(.map)',
+  allLibTests: 'lib/test/**/*.js',
 };
 
 gulp.task('clean', () => del([
@@ -31,7 +34,7 @@ gulp.task('build', ['lint', 'clean'], () =>
     .pipe(gulp.dest(paths.libDir)),
 );
 
-gulp.task('main', ['build'], () =>
+gulp.task('main', ['test'], () =>
   gulp.src(paths.clientEntryPoint)
     .pipe(webpack(webpackConfig))
     .pipe(gulp.dest(paths.distDir)),
@@ -52,4 +55,9 @@ gulp.task('lint', () =>
     .pipe(eslint())
     .pipe(eslint.format())
     .pipe(eslint.failAfterError()),
+);
+
+gulp.task('test', ['build'], () =>
+  gulp.src(paths.allLibTests)
+    .pipe(mocha()),
 );
